2f10d25703e0b88fca22306f54953fd76f96c36d
hama <hama@hama.industries>
2025-11-26 22:45:00 -0800

    initial

---
+++ src/client/java/hama/industries/tmm/construct/TmmConstructDataGenerator.java
@@ -0,0 +1,11 @@
+package hama.industries.tmm.construct;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TmmConstructDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
